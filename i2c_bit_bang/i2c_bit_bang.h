d83773e4941e1596be13a4b5924973cd4ad79c87
dbrock178 <dbrock178@gmail.com>
2026-01-08 14:17:17 -0800

    Added more functionality.

--- i2c_bit_bang/i2c_bit_bang.h
+++ i2c_bit_bang/i2c_bit_bang.h
@@ -1,8 +1,9 @@
 #ifndef I2C_BIT_BANG_H_
 #define I2C_BIT_BANG_H_
 
-#include <stdint.h>
 #include "stm32f411xe.h"
+#include "systick.h"
+#include <stdint.h>
 
 #define I2C_SUCCESS		1
 #define I2C_ERROR		0
@@ -12,6 +13,9 @@ uint8_t i2c_write(uint8_t dev_address,uint8_t data);
 void i2c_start(void);
 void i2c_stop(void);
 uint8_t i2c_get_ack_status(void);
-void write_byte(uint8_t byte);
-
+void i2c_write_byte(uint8_t byte);
+void i2c_read_byte(uint8_t *byte);
+void i2c_send_ack(void);
+void i2c_send_nack(void);
+void i2c_restart(void);
 #endif /* I2C_BIT_BANG_H_ */
